58b7a409d34f97192a0122a51fc35ddd7266b3c6
mrandpz <378313814@qq.com>
2020-12-27 23:17:08 +0800

    init

--- App.tsx
+++ App.tsx
@@ -1,118 +1,142 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react';
+import {View, Text, Button, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare const global: {HermesInternal: null | {}};
-
-const App = () => {
+function HomeScreen({navigation}) {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="update title"
+        onPress={() => navigation.setOptions({title: 'Updated title'})}
+      />
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+      {/* 传递参数 */}
+      <Button
+        title="传递参数"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            itemId: 86,
+            otherParam: 'anything you want here',
+          });
+        }}
+      />
+    </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+function PropsScreen({extraData}: any) {
+  console.log(extraData);
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Props Screen{extraData.a}</Text>
+    </View>
+  );
+}
+
+function NestScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>NestScreen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen({navigation, route}) {
+  // 接收参数
+  const {itemId, otherParam} = route.params;
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      {/* 接收参数处理 */}
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      {/* 再次点击，可以查看没有参数的时候默认参数 */}
+      <Button
+        title="Go to Details... again"
+        // 在当前页面navigate是不会生效的，要用push才会生效
+        // onPress={() => navigation.navigate('Details')}
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      {/* 返回首页，会直接退到首页 */}
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      {/* 回到堆栈的顶端 */}
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+function LogoTitle() {
+  return <Text>自定义头部，也可以放图片</Text>;
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      {/* initialRouteName 默认路由 */}
+      <Stack.Navigator
+        initialRouteName="Home"
+        // 匹配到所有路由
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          // 配置名称
+          options={{
+            title: 'My home',
+            headerStyle: {
+              //单独配置
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#000',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+            // 右侧
+            headerRight: () => (
+              <Button
+                onPress={() => alert('This is a button!')}
+                title="Info"
+                color="#fff"
+              />
+            ),
+          }}
+        />
+        {/* 嵌套路由 */}
+        <Stack.Screen name="NestScreen" component={NestScreen} />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          // 默认参数
+          initialParams={{itemId: 42}}
+          // 组件title
+          options={{headerTitle: (props) => <LogoTitle {...props} />}}
+        />
+        <Stack.Screen name="PropsScreen">
+          {(props) => <PropsScreen {...props} extraData={{a: '1'}} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
---
+++ src/natigator/README.md
@@ -0,0 +1,6 @@
+## 5.x 版本
+1、yarn add @react-navigation/native
+2、yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+3、在mac 平台开发需要执行 npx pod-install ios
+4、yarn add @react-navigation/stack
+5、发现cmd+r没有刷新的时候，可以按cmd+d，实体机就摇手机，安卓ctrl+m
