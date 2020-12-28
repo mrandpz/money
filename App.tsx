4a3e9d859cd03aa067f596c99d250a2f410116ea
mrandpz <378313814@qq.com>
2020-12-28 22:02:18 +0800

    拉起一个表单或者广告弹框

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
  * @Author: Mr.pz
  * @Date: 2020-12-28 21:31:43
  * @Last Modified by: Mr.pz
- * @Last Modified time: 2020-12-28 21:57:44
+ * @Last Modified time: 2020-12-28 22:01:01
  * 从侧边拉出
  */
 
@@ -15,77 +15,63 @@ import {DrawerActions} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
-  return (
-    <Text style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      Home
-    </Text>
-  );
-}
-
-function ProfileScreen({navigation}) {
-  React.useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      console.log('生命周期');
-      // Screen was focused
-      // Do something
-    });
-
-    return unsubscribe;
-  }, [navigation]);
-  return <Text>'Profile'</Text>;
-}
-function SettingsScreen() {
-  useFocusEffect(
-    React.useCallback(() => {
-      // Do something when the screen is focused
-      console.log('生命周期2');
-      return () => {
-        // Do something when the screen is unfocused
-        // Useful for cleanup functions
-      };
-    }, []),
-  );
-  return <Text>'Settings'</Text>;
-}
-
-function DetailsScreen({navigation}) {
+function HomeScreen({navigation}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-      DetailsScreen
+      <Text style={{fontSize: 30}}>This is the home screen!</Text>
+      <Button
+        onPress={() => navigation.navigate('MyModal')}
+        title="Open Modal"
+      />
     </View>
   );
 }
 
-const HomeStack = createDrawerNavigator();
-const SettingsStack = createStackNavigator();
-const Tab = createBottomTabNavigator();
+function DetailsScreen() {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+}
 
+function ModalScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 30}}>This is a modal!</Text>
+      <Button onPress={() => navigation.goBack()} title="Dismiss" />
+    </View>
+  );
+}
+
+const MainStack = createStackNavigator();
+const RootStack = createStackNavigator();
+
+function MainStackScreen() {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={HomeScreen} />
+      <MainStack.Screen name="Details" component={DetailsScreen} />
+    </MainStack.Navigator>
+  );
+}
+
+function RootStackScreen() {
+  return (
+    <RootStack.Navigator mode="modal">
+      <RootStack.Screen
+        name="Main"
+        component={MainStackScreen}
+        options={{headerShown: false}}
+      />
+      <RootStack.Screen name="MyModal" component={ModalScreen} />
+    </RootStack.Navigator>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="First">
-          {() => (
-            <SettingsStack.Navigator>
-              <SettingsStack.Screen
-                name="Settings"
-                component={SettingsScreen}
-              />
-              <SettingsStack.Screen name="Profile" component={ProfileScreen} />
-            </SettingsStack.Navigator>
-          )}
-        </Tab.Screen>
-        <Tab.Screen name="Second">
-          {() => (
-            <HomeStack.Navigator>
-              <HomeStack.Screen name="Home" component={HomeScreen} />
-              <HomeStack.Screen name="Details" component={DetailsScreen} />
-            </HomeStack.Navigator>
-          )}
-        </Tab.Screen>
-      </Tab.Navigator>
+      <RootStackScreen />
     </NavigationContainer>
   );
 }
