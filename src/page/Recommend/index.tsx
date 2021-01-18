162ba2e47b9015cc21dd39b9a9f48985578f2e89
cyanyiyi <cyanyiyi@foxmail.com>
2021-01-18 23:31:22 +0800

    (HomeScreen Reommend): grid

--- src/page/Recommend/index.tsx
+++ src/page/Recommend/index.tsx
@@ -8,8 +8,42 @@
 
 import * as React from 'react';
 import {useState} from 'react';
-import {Text, ScrollView} from 'react-native';
+import {StyleSheet, View, Text, ScrollView} from 'react-native';
 import FlexColumn from 'components/FlexColumn';
+import Grid from 'components/Grid/index';
+import GridItem from 'components/Grid/gridItem';
+
+interface Iinfo {
+  imgSrc: string;
+  price: string;
+  url: string;
+}
+const goodsList: Iinfo[] = [
+  {
+    imgSrc:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190610%2F17%2F1560159102-EYNsuHBOwd.jpg&refer=http%3A%2F%2Fimage.biaobaiju.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612279989&t=202655775905503802227c0d2ca42314',
+    price: '12.8',
+    url: '跳到某个页面',
+  },
+  {
+    imgSrc:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190610%2F17%2F1560159102-EYNsuHBOwd.jpg&refer=http%3A%2F%2Fimage.biaobaiju.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612279989&t=202655775905503802227c0d2ca42314',
+    price: '13.8',
+    url: '跳到某个页面',
+  },
+  {
+    imgSrc:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190610%2F17%2F1560159102-EYNsuHBOwd.jpg&refer=http%3A%2F%2Fimage.biaobaiju.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612279989&t=202655775905503802227c0d2ca42314',
+    price: '14.8',
+    url: '跳到某个页面',
+  },
+  {
+    imgSrc:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190610%2F17%2F1560159102-EYNsuHBOwd.jpg&refer=http%3A%2F%2Fimage.biaobaiju.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612279989&t=202655775905503802227c0d2ca42314',
+    price: '18.8',
+    url: '跳到某个页面',
+  },
+];
 
 export default () => {
   return (
@@ -49,6 +83,11 @@ export default () => {
           },
         ]}
       />
+      <Grid>
+        {goodsList.map((item: Iinfo, index) => {
+          return <GridItem info={item} key={index + item.imgSrc} />;
+        })}
+      </Grid>
     </ScrollView>
   );
 };
